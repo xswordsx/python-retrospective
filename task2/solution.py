7a686020715e9ca05a9fe07ba992b0d9f391cce4
Ivan Latunov <xswordsx@gmail.com>
2013-04-29 10:24:03 +0300

    update 'groupby' function

---
+++ task2/solution.py
@@ -0,0 +1,7 @@
+from collections import defaultdict
+
+def groupby(func, seq):
+    default_dict = defaultdict(list)
+    for iterable in seq:
+        default_dict[func(iterable)].append(iterable)
+    return dict(default_dict)
