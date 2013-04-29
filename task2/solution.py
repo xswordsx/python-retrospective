24557ea283416db766c2492bc32c9b61be88202b
Ivan Latunov <xswordsx@gmail.com>
2013-04-29 16:10:44 +0300

    'groupby' passing PEP8

--- task2/solution.py
+++ task2/solution.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
 
+
 def groupby(func, seq):
     default_dict = defaultdict(list)
     for iterable in seq:
         default_dict[func(iterable)].append(iterable)
     return dict(default_dict)
+
